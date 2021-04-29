3407a07b162b0af464a486ea8acd2d3ba7a0903a
Wael Mohamed <wmoh.mail@gmail.com>
2021-04-29 18:48:22 +0000

    start orders

---
+++ app/Models/admin/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\admin;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+}
---
+++ database/migrations/2021_04_29_181253_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('order_number')->nullable()->unique();
+            $table->enum('payment_method', ['أونلاين', 'الدفع عند الاستلام'])
+                ->default('الدفع عند الاستلام');
+            $table->integer('total')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
