b59b8b067cd93d97044c426a6370ae809cda7e94
Wael Mohamed <wmoh.mail@gmail.com>
2021-05-08 16:56:14 +0000

    small fix to auth

--- app/Http/Controllers/admin/AuthController.php
+++ app/Http/Controllers/admin/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\admin\User;
+use App\Repositories\AuthRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -12,54 +13,27 @@ use Illuminate\Validation\Rule;
 
 class AuthController extends Controller
 {
+
+    protected $authRepository;
+
+    public function __construct(AuthRepository $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
+
     public function edit()
     {
         $user = User::findOrfail(Auth::user()->id);
-
         return view('admin.profile.edit', compact('user'));
     } // End edit
 
     public function update(Request $request)
     {
-        $user = Auth::user();
-
-        $data = $request->validate([
-            'name' => ['bail', 'required', 'string', 'max:50'],
-            'email' => ['bail', 'required', 'email', Rule::unique('users')->ignore($user->id)],
-            'phone' => ['bail', 'nullable', 'max:50' , 'min:6'],
-        ]);
-
-        $user->update($data);
-        $user->save();
-
-        notify()->success(' تحديث البيانات الأساسية','تم بنجاح');
-
-
-        return redirect()->back();
-
-
+        return $this->authRepository->updateInformation($request);
     }
 
     public function changePassword(Request $request)
     {
-        if (!(Hash::check($request->get('old_password'), Auth::user()->password))) {
-            return back()->with('error','كلمة المرور الحالية غير صحيحة');
-        }
-
-        if (strcmp($request->get('old_password'), $request->get('password')) == 0) {
-            return back()->with('error','لا يمكن أن تتطابق كلمة المرور الجديدة مع القديمة');
-        }
-
-        $request->validate([
-            'old_password' => ['bail', 'required'],
-            'password' => ['bail', 'required', 'confirmed', 'min:8']
-        ]);
-
-        $user = Auth::user();
-        $user->password = Hash::make($request->get('password'));
-        $user->save();
-
-        notify()->success('تغيير كلمة مرور الحساب', 'تم بنجاح');
-        return back();
+       return $this->authRepository->updatePassword($request);
     } // End changePassword
 }
