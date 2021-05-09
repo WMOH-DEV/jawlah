e5e182e819b89e19a5ba079c042101e13a8c969d
Wael Mohamed <wmoh.mail@gmail.com>
2021-05-09 00:28:00 +0000

    small fix

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,8 +1,9 @@
 <footer id="page-footer" class="bg-body-light">
   <div class="content py-0">
     <div class="row font-size-sm">
-      <div class="col-sm-6 order-sm-2 mb-1 mb-sm-0 text-center text-sm-left">{{ __('global.made_by') }}
-        <i class="fa fa-heart text-danger"></i> {{ __('global.by') }}
+      <div class="col-sm-6 order-sm-2 mb-1 mb-sm-0 text-center text-sm-left">
+          <span>{{ __('global.by_company') }} <a class="font-w600" href="https://www.marslia.com.eg" target="_blank">Marslia </a></span> -
+          {{ __('global.made_by') }}
          <a class="font-w600" href="{{ __('global.author_link') }}" target="_blank">
           {{ __('global.author') }}</a>
       </div>
